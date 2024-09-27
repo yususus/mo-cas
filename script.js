5f16220c95a5706506bf95cc0b7cf1cb0a9551c7
Yusuf Aydın <yusayd3825@gmail.com>
2024-09-27 14:09:23 +0300

    created

---
+++ script.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const links = document.querySelectorAll('a[href^="#"]');
+    links.forEach(link => {
+        link.addEventListener('click', function (e) {
+            e.preventDefault();
+            const targetId = this.getAttribute('href');
+            const targetSection = document.querySelector(targetId);
+            targetSection.scrollIntoView({
+                behavior: 'smooth'
+            });
+        });
+    });
+});
