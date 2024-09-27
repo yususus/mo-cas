13e470ea86fb46309b804ecdb78e06bf499f8596
Yusuf Aydın <yusayd3825@gmail.com>
2024-09-27 14:43:10 +0300

    update

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Sayfa içi anchor linkler için smooth scroll
 document.addEventListener("DOMContentLoaded", function () {
     const links = document.querySelectorAll('a[href^="#"]');
     links.forEach(link => {
@@ -9,5 +10,37 @@ document.addEventListener("DOMContentLoaded", function () {
                 behavior: 'smooth'
             });
         });
+    });
+
+    // Modal açma ve kapatma işlemleri
+    const modal = document.getElementById('productModal');
+    const closeModalBtn = document.querySelector('.close');
+    const reviewButtons = document.querySelectorAll('.review-button');
+
+    // İncele butonuna tıklandığında modal gösterilecek
+    reviewButtons.forEach(button => {
+        button.addEventListener('click', function() {
+            const productImage = this.getAttribute('data-image');
+            const productDescription = this.getAttribute('data-description');
+
+            // Resim ve açıklama modal içerisine yerleştiriliyor
+            modal.querySelector('img').src = productImage;
+            modal.querySelector('.description').innerText = productDescription;
+
+            // Modal görünür yapılıyor
+            modal.style.display = 'flex';
+        });
+    });
+
+    // Modalı kapatmak için close butonuna tıklandığında
+    closeModalBtn.addEventListener('click', function() {
+        modal.style.display = 'none';
+    });
+
+    // Modal dışına tıklandığında da modal kapatılsın
+    window.addEventListener('click', function(e) {
+        if (e.target === modal) {
+            modal.style.display = 'none';
+        }
     });
 });
