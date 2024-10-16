fce739d628135dff2cdeb736f60fa0e710001cd0
Yusuf Aydın <yusayd3825@gmail.com>
2024-10-16 17:57:55 +0300

    bug fixes navbar

--- js/script.js
+++ js/script.js
@@ -1,46 +1,58 @@
-// Sayfa içi anchor linkler için smooth scroll
-document.addEventListener("DOMContentLoaded", function () {
-    const links = document.querySelectorAll('a[href^="#"]');
-    links.forEach(link => {
-        link.addEventListener('click', function (e) {
-            e.preventDefault();
-            const targetId = this.getAttribute('href');
-            const targetSection = document.querySelector(targetId);
-            targetSection.scrollIntoView({
-                behavior: 'smooth'
+
+// Smooth scroll için
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+
+document.querySelectorAll('.nav-link').forEach(link => {
+    link.addEventListener('click', function(e) {
+        if (this.getAttribute('href') === "#") {
+            e.preventDefault(); // Varsayılan bağlantı davranışını önle
+            window.scrollTo({
+                top: 0,
+                behavior: 'smooth' // Yumuşak kaydırma
             });
-        });
-    });
-
-    // Modal açma ve kapatma işlemleri
-    const modal = document.getElementById('productModal');
-    const closeModalBtn = document.querySelector('.close');
-    const reviewButtons = document.querySelectorAll('.review-button');
-
-    // İncele butonuna tıklandığında modal gösterilecek
-    reviewButtons.forEach(button => {
-        button.addEventListener('click', function() {
-            const productImage = this.getAttribute('data-image');
-            const productDescription = this.getAttribute('data-description');
-
-            // Resim ve açıklama modal içerisine yerleştiriliyor
-            modal.querySelector('img').src = productImage;
-            modal.querySelector('.description').innerText = productDescription;
-
-            // Modal görünür yapılıyor
-            modal.style.display = 'flex';
-        });
-    });
-
-    // Modalı kapatmak için close butonuna tıklandığında
-    closeModalBtn.addEventListener('click', function() {
-        modal.style.display = 'none';
-    });
-
-    // Modal dışına tıklandığında da modal kapatılsın
-    window.addEventListener('click', function(e) {
-        if (e.target === modal) {
-            modal.style.display = 'none';
         }
     });
 });
+
+
+// Modal açma ve kapatma işlemleri
+const modal = document.getElementById('productModal');
+const closeModalBtn = document.querySelector('.close');
+const reviewButtons = document.querySelectorAll('.review-button');
+
+// İncele butonuna tıklandığında modal gösterilecek
+reviewButtons.forEach(button => {
+    button.addEventListener('click', function () {
+        const productImage = this.getAttribute('data-image');
+        const productDescription = this.getAttribute('data-description');
+
+        // Resim ve açıklama modal içerisine yerleştiriliyor
+        modal.querySelector('img').src = productImage;
+        modal.querySelector('.description').innerText = productDescription;
+
+        // Modal görünür yapılıyor
+        modal.style.display = 'flex';
+    });
+});
+
+// Modalı kapatmak için close butonuna tıklandığında
+closeModalBtn.addEventListener('click', function () {
+    modal.style.display = 'none';
+});
+
+// Modal dışına tıklandığında da modal kapatılsın
+window.addEventListener('click', function (e) {
+    if (e.target === modal) {
+        modal.style.display = 'none';
+    }
+});
+
